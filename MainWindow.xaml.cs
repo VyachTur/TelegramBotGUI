883037f89a716e6ea5ec4cef6dbbbbf1de7fb213
Tamplier <tamplier29@mail.ru>
2020-10-26 22:32:31 +1000

    Контрольная точка

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -37,6 +37,27 @@ namespace TelegramBotGUI {
             client = new TgBot_Client(this);
 
 
+            //Border bord = new Border {
+            //    BorderBrush = Brushes.Black,
+            //    BorderThickness = new Thickness(1),
+            //    CornerRadius = new CornerRadius(10),
+            //    Margin = new Thickness(5),
+            //    Background = Brushes.LightGray
+            //};
+
+            //TextBlock txtBl = new TextBlock() {
+            //    Text = "Сообщениецццццццццццццццццццццццццццццццццццццццццццццццццццццццццц",
+            //    MaxWidth = 400,
+            //    TextWrapping = TextWrapping.Wrap
+            //};
+
+            //bord.Child = txtBl;
+
+            Messages msg = new Messages { MessageText = "Пробное сообщениецццццwwwwww  wwwwwwwwwwwwwццццццццццц", MessageTime = DateTime.Now.ToShortTimeString() };
+
+
+            listMessages.Items.Add(msg);
+
         }
     }
 }
---
+++ Messages.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramBotGUI {
+    class Messages {
+
+        public string MessageText { get; set; }
+
+        public string MessageTime { get; set; }
+    }
+}
